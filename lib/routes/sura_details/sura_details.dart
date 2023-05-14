875263f59ac579f3ca8c1ab739f4928dcf0068d5
ahmed <ahmedeltony275@gmail.com>
2023-05-14 03:57:19 +0200

    complete quran tab and some details

--- lib/routes/sura_details/sura_details.dart
+++ lib/routes/sura_details/sura_details.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:isalmi/model/sura_details_args.dart';
 
+import '../../utlis/app_colors.dart';
+
 class SuraDetails extends StatefulWidget {
   static String routeName = "sura details";
 
@@ -10,25 +12,44 @@ class SuraDetails extends StatefulWidget {
 }
 
 class _SuraDetailsState extends State<SuraDetails> {
-  String suraContent="";
+  List<String> suraLines=[];
 
   @override
   Widget build(BuildContext context) {
   var args=ModalRoute.of(context)!.settings.arguments as SuraDetailsArgs;
-  if(suraContent.isEmpty)
+  if(suraLines.isEmpty)
     {
       ReadSuraContent(args.filename);
     };
-  return Scaffold(
-      appBar: AppBar(title: Text(args.suraname,)),
-      body:suraContent.isEmpty ?Center(child: CircularProgressIndicator()):Text(
-       suraContent
-       ),
-       );
+  return Container(
+    decoration: const BoxDecoration(image:DecorationImage(
+        image: AssetImage("assets/bg_home.png"),fit: BoxFit.fill),),
+    child: Scaffold(
+      backgroundColor: Colors.transparent,
+        appBar: AppBar(centerTitle: true,title: Text(args.suraname,
+            textAlign:TextAlign.right,
+            style:TextStyle(color: Appcolours.accentcolor,
+            fontSize:30,fontWeight: FontWeight.bold )),
+          elevation: 0,backgroundColor: Colors.transparent,),
+        body:suraLines.isEmpty ?Center(child:
+        CircularProgressIndicator()):ListView.separated
+          (itemBuilder: (context, index) {
+            return Text("(${index+1}) ${suraLines[index]} ",textAlign:TextAlign.right,
+              style: TextStyle(fontSize:25,fontWeight: FontWeight.bold ),);
+
+        },itemCount: suraLines.length,
+          separatorBuilder: (context, index) {
+            return Divider(height: 5,thickness: 4,color: Appcolours.primarycolor);
+          }
+
+        ),
+         ),
+  );
   }
 
   ReadSuraContent(String filename) async {
-  suraContent=await rootBundle.loadString("assets/quran_files/$filename");
+  var suraContent=await rootBundle.loadString("assets/quran_files/$filename");
+  suraLines=suraContent.trim().split("\n");
   setState(() {});
   }
   }
